67373960f0c6240d7303a5fa4f0274ca87816439
Joel Thomas <joel.thomas@quoscient.io>
2019-05-16 08:30:49 +0200

    Generate smaller qrcode png

--- main.py
+++ main.py
@@ -50,11 +50,11 @@ if __name__ == '__main__':
     qr = qrcode.QRCode(
         version=1,
         error_correction=qrcode.constants.ERROR_CORRECT_L,
-        box_size=10,
-        border=4
+        box_size=5,
+        border=2
     )
     qr.add_data(req)
     qr.make(fit=True)
     img = qr.make_image(fill_color="black", back_color="white")
 
-    img.save('foo.jpg', quality=100)
+    img.save('test.png', quality=100)
