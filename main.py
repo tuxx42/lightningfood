a016bc5dcf9629fce3970865c1b1ccace5b62b34
Joel Thomas <joel.thomas@quoscient.io>
2019-06-07 22:11:50 +0200

    Use remote lnd

--- main.py
+++ main.py
@@ -1,23 +1,22 @@
 import rpc_pb2 as ln
 import rpc_pb2_grpc as lnrpc
-from flask import Flask, render_template, make_response
-from RPi import GPIO
-import time
+from flask import Flask, render_template, make_response, send_from_directory
+# from flask_socketio import SocketIO, emit
+# from RPi import GPIO
 import grpc
 import os
 import codecs
-import threading
-import qrcode
-import io
+# import time
 
 app = Flask(__name__)
 
+MACAROONS_PATH = '/home/alarm/.lnd/data/chain/bitcoin/mainnet/admin.macaroon'
+CERT_PATH = '/home/alarm/.lnd/tls.cert'
+
 
 class LndClient:
-    def __init__(self, host='localhost',
-                 port=10009,
-                 cert_path='~/.lnd/tls.cert',
-                 macaroons_path='~/.lnd/data/chain/bitcoin/mainnet/admin.macaroon'):
+    def __init__(self, host='localhost', port=10009,
+                 cert_path='tls.cert', macaroons_path='admin.macaroon'):
 
         with open(os.path.expanduser(macaroons_path), 'rb') as f:
             macaroon = codecs.encode(f.read(), 'hex')
@@ -52,25 +51,25 @@ class LndClient:
         for invoice in self.stub.SubscribeInvoices(p):
             yield invoice
 
-client = LndClient()
+
+client = LndClient(host='archie')
+
 
 def wait_for_invoice():
-    GPIO.setmode(GPIO.BCM)
-    GPIO.setup(18, GPIO.OUT)
-    p = GPIO.PWM(18, 50)
-
-    for i in client.SubscribeInvoices():
-        p.start(1)
-        p.ChangeDutyCycle(1)
-        time.sleep(0.3)
-        p.ChangeDutyCycle(11)
-        time.sleep(0.3)
-        p.ChangeDutyCycle(1)
-
-
-
-x = threading.Thread(target=wait_for_invoice)
-x.start()
+    pass
+    # for i in client.SubscribeInvoices():
+    #     GPIO.setmode(GPIO.BCM)
+    #     GPIO.setup(18, GPIO.OUT)
+    #     p = GPIO.PWM(18, 50)
+    #     p.start(1)
+    #     p.ChangeDutyCycle(7.5)
+    #     time.sleep(0.3)
+    #     p.ChangeDutyCycle(12.5)
+    #     time.sleep(0.8)
+    #     p.ChangeDutyCycle(7.5)
+    #     time.sleep(1)
+    #     p.stop()
+    #     GPIO.cleanup()
 
 
 @app.route('/')
@@ -80,30 +79,15 @@ def index():
 
 @app.route('/invoice')
 def invoice():
-    data = client.AddInvoice(4000, "chicken food").payment_request
+    data = client.AddInvoice(200, "chicken food").payment_request
     r = make_response(data)
     r.mimetype = 'text/plain'
     return r
 
 
-@app.route('/qr/<invoice>')
-def qr(invoice):
-    qr = qrcode.QRCode(
-        version=1,
-        error_correction=qrcode.constants.ERROR_CORRECT_L,
-        box_size=5,
-        border=2
-    )
-    qr.add_data(invoice)
-    qr.make(fit=True)
-    img = qr.make_image(fill_color="black", back_color="white")
-    bytesio = io.BytesIO()
-    img.save(bytesio, quality=100)
-    data = bytesio.getvalue()
-
-    r = make_response(data)
-    r.mimetype = 'image/png'
-    return r
+@app.route('/js/<path>')
+def send_js(path):
+    return send_from_directory('js', path)
 
 
 if __name__ == '__main__':
